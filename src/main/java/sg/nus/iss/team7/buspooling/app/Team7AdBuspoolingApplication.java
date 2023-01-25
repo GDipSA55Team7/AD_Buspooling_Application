26ac20ab3476c575897689b53bc48fcbd1ef2764
Shaun_Lin <shanfu87@yahoo.com>
2023-01-25 23:40:44 +0800

    created models

---
+++ src/main/java/sg/nus/iss/team7/buspooling/app/Team7AdBuspoolingApplication.java
@@ -0,0 +1,13 @@
+package sg.nus.iss.team7.buspooling.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team7AdBuspoolingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team7AdBuspoolingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/nus/iss/team7/buspooling/app/Team7AdBuspoolingApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.nus.iss.team7.buspooling.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team7AdBuspoolingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
